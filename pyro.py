6165fb4a620fc7013c865a016b3b3bc480c08e13
meaall-com <67676419+meaall-com@users.noreply.github.com>
2021-12-04 21:12:05 +0300

    Update pyro.py

--- pyro.py
+++ pyro.py
@@ -5,7 +5,7 @@ app = Client("gramsession", 14897586, "49db49b537577139a4b337f2764b70a3")
 
 @app.on_message(filters.channel)
 def my_function(client, message):
-    if message.chat.id != -1001159025906:
+    if message.sender_chat.id != -1001159025906 or message.sender_chat.id != -1001780843936:
         print(message)
         message.forward("@meaallh_for")
 
